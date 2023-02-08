8e7a750b72631c85955b572afe5aadd649a56b45
anatolegerasimov <anatole82@mail.ru>
2023-02-08 17:09:10 +0300

    New commit

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ per_cent_list = list(per_cent.values())
 new_list = [i * money for i in per_cent_list]
 deposit = list(map(int, new_list))
 print("deposit - ", deposit)
-print("Максимальная доходность - ", max(deposit))
+print("Максимальная сумма, которую вы можете заработать - ", max(deposit))
