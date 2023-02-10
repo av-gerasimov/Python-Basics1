f1b46c0dd946524722ac277bc0fe0fe139d29faa
anatolegerasimov <anatole82@mail.ru>
2023-02-10 16:22:50 +0300

    New commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 money = float(input("Введите сумму: "))
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 per_cent_list = list(per_cent.values())
-new_list = [i * money for i in per_cent_list]
+new_list = [i * money / 100 for i in per_cent_list]
 deposit = list(map(int, new_list))
 print("deposit - ", deposit)
 print("Максимальная сумма, которую вы можете заработать - ", max(deposit))
